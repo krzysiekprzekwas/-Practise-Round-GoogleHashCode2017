d0ead320fcc86e24c90a0c70fc7d301f25e7a207
Krzysiek <Krzysiek@DESKTOP-9QISGFU>
2017-02-22 20:51:57 +0100

    Cleaned up a bit

--- HashCodeGoogle/Program.cs
+++ HashCodeGoogle/Program.cs
@@ -7,102 +7,148 @@ using System.Linq;
 
 namespace HashCodeGoogle
 {
-    class Program
+    internal class Program
     {
-        static int rows;
-        static int columns;
-        static int minIngredient;
-        static int maxCells;
-        private static int[,] array;
+        private static int _rows;
+        private static int _columns;
+        private static int _minIngredient;
+        private static int _maxCells;
+        private static int[,] _array;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
 
             using (TextReader reader = File.OpenText("small.in"))
             {
-                DividePizza(reader, "smallResults.txt");
+                var slices = DividePizza(reader);
+                if (slices.Equals(null))
+                {
+                    Console.WriteLine("Input File is incorrect!");
+                    Console.ReadKey();
+                    return;
+                }
+
+                SaveResults("smallResults.txt", slices);
             }
 
             using (TextReader reader = File.OpenText("medium.in"))
             {
-                DividePizza(reader, "mediumResults.txt");
+                var slices = DividePizza(reader);
+                if (slices.Equals(null))
+                {
+                    Console.WriteLine("Input File is incorrect!");
+                    Console.ReadKey();
+                    return;
+                }
+                SaveResults("mediumResults.txt", slices);
             }
 
             using (TextReader reader = File.OpenText("big.in"))
             {
-                DividePizza(reader, "bigResults.txt");
+                var slices = DividePizza(reader);
+                if (slices.Equals(null))
+                {
+                    Console.WriteLine("Input File is incorrect!");
+                    Console.ReadKey();
+                    return;
+                }
+                SaveResults("bigResults.txt", slices);
             }
 
             using (TextReader reader = File.OpenText("example.in"))
             {
-                DividePizza(reader, "example.txt");
+                var slices = DividePizza(reader);
+                if (slices.Equals(null))
+                {
+                    Console.WriteLine("Input File is incorrect!");
+                    Console.ReadKey();
+                    return;
+                }
+                SaveResults("exampleResults.txt", slices);
             }
 
         }
 
-        private static void DividePizza(TextReader reader, string output)
+        private static List<Tuple<int, int, int, int>> DividePizza(TextReader reader)
         {
             // Read data from small.in and initialize Lists
-            ReadInputData(reader);
+            var readOk = ReadInputData(reader);
 
+            // If file wasn't read correctly, abort
+            if (readOk == false)
+                return null;
+
+            // Generate all possible shapes
             var possibleShapes = GeneratePossibleShapes();
 
+            // Get same shapes but reversed order
             var possibleShapesReverted = possibleShapes.ToList();
             possibleShapesReverted.Reverse();
 
-            
+            // Varables for future cutted slices
             var pizzaSlices = new List<Tuple<int, int, int, int>>();
             var pizzaSlicesReverted = new List<Tuple<int, int, int, int>>();
 
-            var tempArray = new int[rows,columns];
+            // Copy pizza to new array
+            var tempArray = new int[_rows,_columns];
+            Array.Copy(_array,tempArray,_array.Length);
 
-            Array.Copy(array,tempArray,array.Length);
+            // Caclculate how many pieces you can cut off using normal order of shapes
+            var result1 = CheckPermutation(possibleShapes, tempArray, pizzaSlices); // TEMP ARRAY IS MODIFIED
 
-            var result1 = CheckPermutation(possibleShapes, tempArray, pizzaSlices);
-
-            Array.Copy(array, tempArray, array.Length);
+            // Copy pizza once again
+            Array.Copy(_array, tempArray, _array.Length);
+            // Caclculate how many pieces you can cut off using reversed order of shapes
             var result2 = CheckPermutation(possibleShapesReverted, tempArray, pizzaSlicesReverted);
 
-            SaveResults(output, result1 > result2 ? pizzaSlices : pizzaSlicesReverted);
+            // Return better set of slices
+            return result1 > result2 ? pizzaSlices : pizzaSlicesReverted;
         }
 
-        private static int CheckPermutation(List<Point> possibleShapes, int[,] tempArray, List<Tuple<int, int, int, int>> pizzaSlices)
+        private static int CheckPermutation(List<Point> possibleShapes, int[,] tempArray, ICollection<Tuple<int, int, int, int>> pizzaSlices)
         {
-            for (int i = 0; i < rows; i++)
+            for (var i = 0; i < _rows; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (var j = 0; j < _columns; j++)
                 {
-                    if (array[i, j] == 0) // If that place was already choped off then move on to the next one
+                    if (_array[i, j] == 0) // If that place was already choped off then move on to the next one
                         continue;
 
                     foreach (var shape in possibleShapes)
                     {
-                        // Count sum of slice in current shape if placed in place IxJ
+                        // Cont number of ingredients pairs slice in current shape if placed in place IxJ
                         var sumCurrent = SumCurrent(shape, i, j, tempArray);
 
-                        // Check if sum is correct. More explanations on top. 
-                        if (sumCurrent >= minIngredient)
-                        {
-                            // Add new slice to others
-                            pizzaSlices.Add(new Tuple<int, int, int, int>(i, j, i + shape.X - 1, j + shape.Y - 1));
+                        // Check if it's not enough ingredients try next shape
+                        if (sumCurrent < _minIngredient) continue;
 
-                            // Erase slice from pizza matrix
-                            CutSliceFromPizza(shape, i, j, tempArray);
+                        // Add new slice to list
+                        pizzaSlices.Add(new Tuple<int, int, int, int>(i, j, i + shape.X - 1, j + shape.Y - 1));
 
-                            // Move to next not choped off place
-                            break;
-                        }
+                        // Erase slice from pizza matrix
+                        CutSliceFromPizza(shape, i, j, tempArray);
+
+                        // Move to next not choped off place
+                        break;
                     }
                 }
             }
 
-            int result = 0;
+            // Calculate how many pieces you menaged to cut off
+            var result = CalculateResult(tempArray);
 
-            for (int i = 0; i < rows; i++)
+
+            return result;
+        }
+
+        private static int CalculateResult(int[,] tempArray)
+        {
+            var result = 0;
+            for (var i = 0; i < _rows; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (var j = 0; j < _columns; j++)
                 {
+                    // If piece is cut off then increase result
                     if (tempArray[i, j] == 0)
                         ++result;
                 }
@@ -110,29 +156,21 @@ namespace HashCodeGoogle
             return result;
         }
 
-        private static void PrintPossibleShapes(List<Point> possibleShapes )
-        {
-            for (int i = 0; i < possibleShapes.Count; i++)
-            {
-                Console.SetCursorPosition(columns + 5, i);
-                Console.WriteLine(possibleShapes.ElementAt(i).X + " x " + possibleShapes.ElementAt(i).Y);
-            }
-        }
-
         private static void CutSliceFromPizza(Point shape, int i, int j, int[,] array)
         {
-            for (int l = 0; l < shape.X; l++)
+            for (var l = 0; l < shape.X; l++)
             {
-                for (int f = 0; f < shape.Y; f++)
+                for (var f = 0; f < shape.Y; f++)
                 {
+                    // Set piece to cut off state
                     array[i + l, j + f] = 0;
                 }
             }
         }
 
-        private static void SaveResults(string output, List<Tuple<int, int, int, int>> pizzaSlices)
+        private static void SaveResults(string output, IReadOnlyCollection<Tuple<int, int, int, int>> pizzaSlices)
         {
-            System.IO.StreamWriter file = new System.IO.StreamWriter(output);
+            var file = new System.IO.StreamWriter(output);
             file.WriteLine(pizzaSlices.Count);
 
             foreach (var pizza in pizzaSlices)
@@ -147,86 +185,71 @@ namespace HashCodeGoogle
         {
             int tomatoCounter = 0, mushroomCounter = 0;
             
-
-            for (int l = 0; l < shape.X; l++)
+            for (var l = 0; l < shape.X; l++)
             {
-                for (int f = 0; f < shape.Y; f++)
+                for (var f = 0; f < shape.Y; f++)
                 {
-                    if (i + l >= rows || j + f >= columns || array[i + l, j + f] == 0)
+                    if (i + l >= _rows || j + f >= _columns || array[i + l, j + f] == 0)
                         return 0;
 
-                    switch (array[i + l, j + f])
+                    if (array[i + l, j + f] == 1)
+                        ++mushroomCounter;
+                    else if (array[i + l, j + f] == 2)
                     {
-                        case 1:
-                            ++mushroomCounter;
-                            break;
-                        case 2:
-                            ++tomatoCounter;
-                            break;
+                        ++tomatoCounter;
                     }
                 }
             }
+
+            // Return number of pars tomato-mushroom
             return Math.Min(tomatoCounter,mushroomCounter);
         }
 
-        private static void ReadInputData(TextReader reader)
+        private static bool ReadInputData(TextReader reader)
         {
-            string text = reader.ReadLine();
-            string[] bits = text.Split(' ');
+            var text = reader.ReadLine();
+            if (text == null)
+                return false;
 
-            rows = int.Parse(bits[0]);
-            columns = int.Parse(bits[1]);
-            minIngredient = int.Parse(bits[2]);
-            maxCells = int.Parse(bits[3]);
+            var bits = text.Split(' ');
 
-            array = new int[rows, columns];
+            _rows = int.Parse(bits[0]);
+            _columns = int.Parse(bits[1]);
+            _minIngredient = int.Parse(bits[2]);
+            _maxCells = int.Parse(bits[3]);
+            
 
-            for (int i = 0; i < rows; i++)
+            _array = new int[_rows, _columns];
+
+            for (int i = 0; i < _rows; i++)
             {
                 text = reader.ReadLine();
-                for (int j = 0; j < columns; j++)
+                if (text == null)
+                    return false;
+                for (int j = 0; j < _columns; j++)
                 {
                     if (text.ElementAt(j) == 'M')
-                        array[i, j] = 1;
+                        _array[i, j] = 1;
                     else if (text.ElementAt(j) == 'T')
-                        array[i, j] = 2;
+                        _array[i, j] = 2;
                 }
             }
+            return true;
         }
 
         private static List<Point> GeneratePossibleShapes()
         {
             List<Point> possibleShapes = new List<Point>();
-            for (int i = 0; i <= maxCells; i++)
+            for (int i = 0; i <= _maxCells; i++)
             {
-                for (int j = 0; j <= maxCells; j++)
+                for (int j = 0; j <= _maxCells; j++)
                 {
-                    if (i * j <= maxCells && i * j >= minIngredient * 2)
+                    if (i * j <= _maxCells && i * j >= _minIngredient * 2)
                         possibleShapes.Add(new Point(i, j));
                 }
             }
 
             return possibleShapes;
-        }
-
-        private static void PrintPizza()
-        {
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
-                {
-                    Console.Write(array[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private static void PrintCursor(int l, int m, ConsoleColor color)
-        {
-            Console.SetCursorPosition(m,l);
-            Console.ForegroundColor = color;
-            Console.Write(array[l, m]);
-            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
